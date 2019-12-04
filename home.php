80fef0ec04052ae88b51490d3fc04edebb8b70ca
Ambar Mutha <ambar.ytl@gmail.com>
2019-12-04 14:46:38 +0530

    add separate header file - update webpack config - php include header in home -  update start command

---
+++ home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Shrine (MDC Web Example App)</title>
+    <link rel="shortcut icon" href="https://material.io/favicon.ico" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <link rel="stylesheet" href="dist/bundle-home.css" />
+</head>
+
+<body class="home">
+    <?php include_once('header/header.php') ?>
+    <div style="height: 200vh; width: 100vw;"></div>
+
+    <script src="dist/bundle-home.js" async></script>
+</body>
+
+</html>
