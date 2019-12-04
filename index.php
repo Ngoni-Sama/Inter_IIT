146d3b8b3fb54cfc2689140690c457fb0606bc5d
Ambar Mutha <ambar.ytl@gmail.com>
2019-12-04 17:39:26 +0530

    add forgot password link

--- index.php
+++ index.php
@@ -1,75 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<title>Title</title>
-		<link rel="shortcut icon" href="https://material.io/favicon.ico" />
-		<link
-			rel="stylesheet"
-			href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css"
-		/>
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
-		<link rel="stylesheet" href="dist/bundle-login.css" />
-	</head>
 
-	<body class="shrine-login">
-		<section class="header">
-			<img
-				src="https://upload.wikimedia.org/wikipedia/commons/5/51/Logo_of_IIT_Bhilai.png"
-				alt="logo"
-				class="logo"
-			/>
-			<h1>Login</h1>
-		</section>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Title</title>
+    <link rel="shortcut icon" href="https://material.io/favicon.ico" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.0/normalize.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" />
+    <link rel="stylesheet" href="dist/bundle-login.css" />
+</head>
 
-		<form action="home.php">
-			<div class="mdc-text-field username">
-				<input
-					type="text"
-					class="mdc-text-field__input"
-					id="username-input"
-					name="username"
-					required
-				/>
-				<label class="mdc-floating-label" for="username-input">Username</label>
-				<div class="mdc-line-ripple"></div>
-			</div>
-			<div class="mdc-text-field password">
-				<input
-					type="password"
-					class="mdc-text-field__input"
-					id="password-input"
-					name="password"
-					minlength="8"
-					required
-				/>
-				<label class="mdc-floating-label" for="password-input">Password</label>
-				<div class="mdc-line-ripple"></div>
-			</div>
-			<div class="button-container">
-				<button type="button" class="mdc-button forgot">
-					<div class="mdc-button__ripple"></div>
-					<span class="mdc-button__label">
-						Forgot Password
-					</span>
-				</button>
-				<!-- <button type="button" class="mdc-button cancel">
-					<div class="mdc-button__ripple"></div>
-					<span class="mdc-button__label">
-						Sign Up
-					</span>
-				</button> -->
-				<button class="mdc-button mdc-button--raised next">
-					<div class="mdc-button__ripple"></div>
-					<span class="mdc-button__label">
-						Login
-					</span>
-				</button>
-			</div>
-			<div class="button-container"></div>
-		</form>
+<body class="shrine-login">
+    <section class="header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Logo_of_IIT_Bhilai.png" alt="logo" class="logo" />
+        <h1>Login</h1>
+    </section>
 
-		<script src="./dist/bundle-login.js" async></script>
-	</body>
+    <form action="home.php">
+        <div class="mdc-text-field username">
+            <input type="text" class="mdc-text-field__input" id="username-input" name="username" required />
+            <label class="mdc-floating-label" for="username-input">Username</label>
+            <div class="mdc-line-ripple"></div>
+        </div>
+        <div class="mdc-text-field password">
+            <input type="password" class="mdc-text-field__input" id="password-input" name="password" minlength="8" required />
+            <label class="mdc-floating-label" for="password-input">Password</label>
+            <div class="mdc-line-ripple"></div>
+        </div>
+        <div class="button-container">
+            <a href="signup.php" class="mdc-button register">
+                <div class="mdc-button__ripple"></div>
+                <span class="mdc-button__label">
+                    Register
+                </span>
+            </a>
+            <button class="mdc-button mdc-button--raised next">
+                <div class="mdc-button__ripple"></div>
+                <span class="mdc-button__label">
+                    Login
+                </span>
+            </button>
+        </div>
+        <div class="forgot">
+            <a href="#">Forgot Password?</a>
+        </div>
+    </form>
+
+    <script src="./dist/bundle-login.js" async></script>
+</body>
+
 </html>
--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 import { MDCTextField } from '@material/textfield';
 import { MDCRipple } from '@material/ripple';
 
-new MDCRipple(document.querySelector('.forgot'));
+new MDCRipple(document.querySelector('.register'));
 new MDCRipple(document.querySelector('.next'));
 
 const username = new MDCTextField(document.querySelector('.username'));
