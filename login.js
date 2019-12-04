cefae20216b9d520c5ff73aa755e9f0391090734
Ambar Mutha <ambar.ytl@gmail.com>
2019-12-04 15:47:02 +0530

    minor changes to login

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 import { MDCTextField } from '@material/textfield';
 import { MDCRipple } from '@material/ripple';
 
-new MDCRipple(document.querySelector('.cancel'));
+new MDCRipple(document.querySelector('.forgot'));
 new MDCRipple(document.querySelector('.next'));
 
 const username = new MDCTextField(document.querySelector('.username'));
