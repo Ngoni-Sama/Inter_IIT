09ee4693635c466fa4a7120a4d5bdf7c9ff6c69c
ashutosh321607 <ashutoshg@iitbhilai.ac.in>
2019-12-07 20:39:08 +0530

    home page created

--- includes/head.php
+++ includes/head.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <script async src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Damion&display=swap" rel="stylesheet">
