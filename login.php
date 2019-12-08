e3a0b8d6bcd10223a145aad3f1ac5bd52d7edb23
Ambar Mutha <ambar.ytl@gmail.com>
2019-12-08 19:19:36 +0530

    style error text

--- login.php
+++ login.php
@@ -1,37 +1,34 @@
 <?php
-    require('db.php');
-    $login = 1;
-    if(isset($_POST['submit'])){
-        $email = trim($_POST['email']);
-        $password = trim($_POST['password']);
-        $query = 'select * from users where email = "'.$email.'"';
-        $result = mysqli_query($conn, $query);
-        $str = '';
-        if($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                if($password ===$row['passkey']){
-                    $str = "Successfully login.";
-                    $login = 1;
-                    header('Location: working.php');
-                    exit();
-                }
+require('db.php');
+$login = 1;
+if (isset($_POST['submit'])) {
+    $email = trim($_POST['email']);
+    $password = trim($_POST['password']);
+    $query = 'select * from users where email = "' . $email . '"';
+    $result = mysqli_query($conn, $query);
+    $str = '';
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if ($password === $row['passkey']) {
+                $str = "Successfully login.";
+                $login = 1;
+                header('Location: working.php');
+                exit();
             }
         }
-
-        else{
-            $str =  "Invalid Email or Password.";
-            $login = 0;
-        }
-
-        if(empty($_POST['email'])){
-	        $str = "UserName is empty!";
-            $login = 0;
-	    }
-        else if(empty($_POST['password'])){
-            $str = "Password is empty!";
-            $login = 0;
-	    }
+    } else {
+        $str =  "Incorrect Email or Password.";
+        $login = 0;
     }
+
+    if (empty($_POST['email'])) {
+        $str = "Email is empty!";
+        $login = 0;
+    } else if (empty($_POST['password'])) {
+        $str = "Password is empty!";
+        $login = 0;
+    }
+}
 ?>
 
 
@@ -52,7 +49,7 @@
     <?php include('includes/navbar.php') ?>
 
     <main>
-        <form class="login-card z-depth-1" action = "<?php $_PHP_SELF ?>" method = "POST">
+        <form class="login-card z-depth-1" action="<?php $_PHP_SELF ?>" method="POST">
             <h2>Login</h2>
             Use Your Registered Email
             <div class="input-field">
@@ -63,9 +60,9 @@
                 <input id="password" type="password" class="validate" name="password">
                 <label for="password">Password</label>
             </div>
-            <?php if($login == 0){?>
-                <div style="background-color : red;">
-                    <?php echo $str?>
+            <?php if ($login == 0) { ?>
+                <div class="red-text" style="margin-bottom: 10px;">
+                    <?php echo $str ?>
                 </div>
             <?php } ?>
             <button class="btn submit-btn z-depth-0 waves-effect" name="submit">
