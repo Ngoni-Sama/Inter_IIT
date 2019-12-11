5177fec397f4fbc0df323197a81bc6261a3a2b62
Ambar Mutha <ambar.ytl@gmail.com>
2019-12-11 22:48:51 +0530

    change button styling

--- includes/navbar.php
+++ includes/navbar.php
@@ -12,11 +12,11 @@
                 <li><a href="./shop.php">Order Medicines</a></li>
                 <li><a href="collapsible.html">Reminder</a></li>
                 <li><a href="mobile.html">About Us</a></li>
-                <?php if(!isset($_COOKIE['login'])):?>
-                    <li><a href="login.php"  class="waves-effect waves-light btn-small">Login</a></li>
-                <?php else:?>
-                    <li><a href="logout.php"  class="waves-effect waves-light btn-small">Logout</a></li>
-                <?php endif;?>
+                <?php if (!isset($_COOKIE['login'])) : ?>
+                    <li><a href="login.php" class="waves-effect black white-text btn-flat btn-small">Login</a></li>
+                <?php else : ?>
+                    <li><a href="logout.php" class="waves-effect black white-text btn-flat btn-small">Logout</a></li>
+                <?php endif; ?>
 
             </ul>
         </div>
@@ -38,33 +38,31 @@
 </div>
 
 <ul class="sidenav" id="mobile-demo">
-    <?php if(isset($_COOKIE['login'])) {
-        if($_COOKIE['login']==1){?>
+    <?php if (isset($_COOKIE['login'])) {
+        if ($_COOKIE['login'] == 1) { ?>
             <h6 style="background-color:aquamarine; padding-top : 15px; padding-bottom:20px; padding-left: 25px; ">Hello, <?php echo $_COOKIE['name'] ?></h6>
             <li><a href="./">Home</a></li>
             <li><a href="./shop.php">Order Medicines</a></li>
             <li><a href="">Reminder</a></li>
             <hr>
             <li><a href="">About Us</a></li>
-            <li><a href="logout.php"  class="waves-effect waves-light btn-small">Logout</a></li>
-    <?php }
-        else{ ?>
+            <li><a href="logout.php" class="waves-effect btn-flat teal white-text btn-small">Logout</a></li>
+        <?php } else { ?>
             <h6 style="background-color:aquamarine; padding-top : 15px; padding-bottom:20px; padding-left: 25px; ">Hello, Guest</h6>
             <li><a href="./">Home</a></li>
             <li><a href="./shop.php">Order Medicines</a></li>
             <li><a href="">Reminder</a></li>
             <hr>
             <li><a href="">About Us</a></li>
-            <li><a href="login.php"  class="waves-effect waves-light btn-small">Login</a></li>
-       <?php } 
-    }
-    else {?>   
+            <li><a href="login.php" class="waves-effect btn-small">Login</a></li>
+        <?php }
+        } else { ?>
         <h6 style="background-color:aquamarine; padding-top : 15px; padding-bottom:20px; padding-left: 25px; ">Hello, Guest</h6>
         <li><a href="./">Home</a></li>
         <li><a href="./shop.php">Order Medicines</a></li>
         <li><a href="">Reminder</a></li>
         <hr>
         <li><a href="">About Us</a></li>
-        <li><a href="login.php"  class="waves-effect waves-light btn-small">Login</a></li>
+        <li><a href="login.php" class="waves-effect btn-flat teal white-text btn-small">Login</a></li>
     <?php }  ?>
 </ul>
