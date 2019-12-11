440a2b76b8f2828243aad1f86b145928d9364533
Ambar Mutha <ambar.ytl@gmail.com>
2019-12-11 17:23:15 +0530

    add display order page change list border radius

---
+++ js/order.js
@@ -0,0 +1,48 @@
+const listElement = document.querySelector('#list');
+const listItemTemplate = document.querySelector('#list-item');
+
+let order = {
+	id: '29382361',
+	status: 'Delivered on Dec 12, 19',
+	amount: 10000,
+	items: [
+		{
+			name: 'Medicine A',
+			price: 1000,
+			quantity: 5,
+			company: 'ABC Pharmaceuticals',
+			mfg: '10/11/12',
+			exp: '10/11/13'
+		},
+		{
+			name: 'Medicine B',
+			price: 1000,
+			quantity: 5,
+			company: 'ABC Pharmaceuticals',
+			mfg: '10/11/12',
+			exp: '10/11/13'
+		}
+	]
+};
+
+function displayOrder(order) {
+	listElement.querySelector('.order-id').textContent = order.id;
+	listElement.querySelector('.status').textContent = order.status;
+	listElement.querySelector('.grand-total').textContent = order.amount;
+
+	order.items.forEach(item => {
+		const newListItemElement = document.importNode(listItemTemplate.content, true);
+
+		newListItemElement.querySelector('.medicine-name').textContent = item.name;
+		newListItemElement.querySelector('.price').textContent = item.price;
+		newListItemElement.querySelector('.quantity').textContent = item.quantity;
+		newListItemElement.querySelector('.company').textContent = item.company;
+		newListItemElement.querySelector('.mfg').textContent = item.mfg;
+		newListItemElement.querySelector('.exp').textContent = item.exp;
+		newListItemElement.querySelector('.net-price').textContent = item.quantity * item.price;
+
+		listElement.appendChild(newListItemElement);
+	});
+}
+
+displayOrder(order);
---
+++ order.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include_once('includes/head.php'); ?>
+    <title>MedicoRx | My Orders</title>
+
+    <style>
+        .collection-item,
+        .collection-header {
+            display: flex;
+            align-items: center;
+        }
+
+        .bold {
+            font-weight: bold;
+            font-size: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <?php include_once('includes/navbar.php'); ?>
+
+    <div class="container" style="max-width: 800px;">
+        <ul id="list" class="collection with-header">
+            <li class="collection-header">
+                <div style="flex-grow: 1">
+                    <h4>Order #<span class="order-id">723812</span></h4>
+                    Status: <span class="status">Delivered on Dec 11, 19</span> <br> <br>
+                    <a href="#" class="btn waves-effect teal white-text btn-flat">Return Medicines</a>
+                </div>
+                <div class="green-text bold">
+                    ₹<span class="grand-total">20000</span>
+                </div>
+            </li>
+        </ul>
+    </div>
+
+    <template id="list-item">
+        <li class="collection-item avatar waves-effect">
+            <div style="flex-grow: 1">
+
+                <i class="fas fa-pills circle teal"></i>
+                <div class="medicine-name">Medicine Name</div>
+                <p>
+                    <span class="green-text">
+                        ₹ <span class="price">100</span>
+                    </span>
+                    &ThickSpace; Quantity:
+                    <span class="quantity">10</span> <br>
+                    <span class="blue-text company">ABC Pharmaceuticals</span><br>
+                    Mfg: <span class="mfg">10/11/19</span><br>
+                    Exp: <span class="exp">10/11/20</span>
+                </p>
+            </div>
+
+            <div class="bold green-text">
+                ₹<span class="net-price">1000</span>
+            </div>
+        </li>
+    </template>
+
+    <?php include_once('includes/scripts.php'); ?>
+    <script src="js/order.js"></script>
+</body>
+
+</html>
